f9698bfcd5989ceb640b0aa2866d54c438e16cd1
Fabian Fabro <fabrofabian@gmail.com>
2019-04-26 05:06:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ def find_the_cheese(foodies)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  foodies.include?(cheese_types)
+  foodies.include? cheese_types
 
 
 end
