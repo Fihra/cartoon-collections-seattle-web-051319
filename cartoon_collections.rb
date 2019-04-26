d7e89810869af0d395b99a3aba307faf4836ab19
Fabian Fabro <fabrofabian@gmail.com>
2019-04-26 04:20:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,13 @@ def long_planeteer_calls(calls)# code an argument here
   }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(foodies)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  if cheese_types.include?(foodies)
+    return cheese_types.index(foodies)
+  else
+    return nil
+  end
 end
