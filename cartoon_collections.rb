41af3bfe96ba18b64860217d8da03b3506898b4a
Fabian Fabro <fabrofabian@gmail.com>
2019-04-26 04:00:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,10 +14,8 @@ end
 
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
-  calls.each do {|c| if c.length >= 4 ? return false: return true
-
-  }
-
+  calls.each do {|c| if c.length >= 4 ? return false: return true  }
+  end
 end
 
 def find_the_cheese# code an argument here
